97fe7e19d1f16c18acf80346fb1245e2152295de
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-13 21:53:23 +0900

    feat: ask your input and trim for remove white space

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
 use std::io;
 
 fn main() {
+  println!("Enter your weight (kg): ");
   let mut input = String::new();
 
-  io::stdin().read_line(&mut input);
+  // add unwrap if result is error terminate excution
+  io::stdin().read_line(&mut input).unwrap();
 
-  borrow_string(&input);
-  own_string(input);
+  // prevent white space
+  let weight = input.trim();
+  dbg!(weight);
 
-  // println!("input: {}", input);
-  // let mut mars_weight = calculate_weight_on_mars(100.0);
-  // println!("Weight on Mars: {}kg", mars_weight);
+  println!("input: {}", input);
+  let mut mars_weight = calculate_weight_on_mars(100.0);
+  println!("Weight on Mars: {}kg", mars_weight);
 }
 
 
-fn borrow_string(s: &String) {
-  println!("{}", s);
-}
-
-fn own_string(s: String) {
-  println!("{}", s);
-}
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
   (weight / 9.81) * 3.711
