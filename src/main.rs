2969ea6f682dccdc130bf7a1c10d2898b986d50f
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-13 21:46:47 +0900

    feat: ownership and borrowing more

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ fn main() {
   let mut input = String::new();
 
   io::stdin().read_line(&mut input);
-  println!("input: {}", input);
-  let mut mars_weight = calculate_weight_on_mars(100.0);
-  println!("Weight on Mars: {}kg", mars_weight);
+
+  borrow_string(&input);
+  own_string(input);
+
+  // println!("input: {}", input);
+  // let mut mars_weight = calculate_weight_on_mars(100.0);
+  // println!("Weight on Mars: {}kg", mars_weight);
+}
+
+
+fn borrow_string(s: &String) {
+  println!("{}", s);
+}
+
+fn own_string(s: String) {
+  println!("{}", s);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
