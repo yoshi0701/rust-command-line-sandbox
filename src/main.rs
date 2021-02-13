2919bddb0167500af04ef20a0ab51640af300f38
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-13 21:01:14 +0900

    feat: mutable and immutable borrow

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ use std::io;
 
 fn main() {
   let mut input = String::new();
-  // when reached some_fn ownership is moved, then input is no longer available
+
+  let mut s1 = &mut input;
+  let s2 = &input;
+  // -> muttable borrow and immutable borrow will be confict
+  println!("{} {}", s1, s2);
+
   some_fn(&mut input);
   io::stdin().read_line(&mut input);
   let mut mars_weight = calculate_weight_on_mars(100.0);
@@ -13,14 +18,6 @@ fn calculate_weight_on_mars(weight: f32) -> f32 {
   (weight / 9.81) * 3.711
 }
 
-// not transfering ownership, ref is the way to reference
 fn some_fn(s: &mut String) {
-  // immutable by default thus this leads to error
   s.push_str("a");
 }
-
-// 1. each value in Rust is owner by a variable
-
-// 2. when the owner goes out of scope, the value will be deallocated
-
-// 3. there can only bt one owner at a time
