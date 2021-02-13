e2c6626815a215ccae1a8a0706c92d4778750c7b
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-13 20:41:31 +0900

    feat: referencing variable

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main() {
   let mut input = String::new();
   // when reached some_fn ownership is moved, then input is no longer available
-  some_fn(input);
+  some_fn(&input);
   io::stdin().read_line(&mut input);
   let mut mars_weight = calculate_weight_on_mars(100.0);
   println!("Weight on Mars: {}kg", mars_weight);
@@ -13,7 +13,11 @@ fn calculate_weight_on_mars(weight: f32) -> f32 {
   (weight / 9.81) * 3.711
 }
 
-fn some_fn(s: String) {}
+// not transfering ownership, ref is the way to reference
+fn some_fn(s: &String) {
+  // immutable by default thus this leads to error
+  s.push_str("a");
+}
 
 // 1. each value in Rust is owner by a variable
 
