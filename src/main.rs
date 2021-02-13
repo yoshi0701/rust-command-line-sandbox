a591c70192f608958eaafb4d55bdd143c692f1e1
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-13 21:25:19 +0900

    feat: stdin sample

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,12 @@ use std::io;
 fn main() {
   let mut input = String::new();
 
-  let s1 = &input;
-  let s2 = &input;
-
-  some_fn(&mut input);
-  // mutable borrow issue happen due to imput passed to fn
-  println!("{} {}", s1, s2);
   io::stdin().read_line(&mut input);
+  println!("input: {}", input);
   let mut mars_weight = calculate_weight_on_mars(100.0);
   println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
   (weight / 9.81) * 3.711
-}
-
-fn some_fn(s: &mut String) {
-  s.push_str("a");
 }
